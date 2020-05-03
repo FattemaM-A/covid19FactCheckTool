3342755cd48a88a59ff2a4e6b50840b71077d28d
FattemaM-A <64709275+FattemaM-A@users.noreply.github.com>
2020-05-03 14:48:39 -0300

    index1.php
    Hey Tara, I'm not entirely sure how to use GitHub and I wasn't able to edit the codes directly but I was able to copy the code and edit it from another source. This is what I have so far. I'm sorry if it's not much. I have been having trouble figuring out how to use HTML but I think I'm getting the hang of it. I will continue to work on the web page and try to get a little more done now that I have a better understanding of how to use HTML. Like I said before I wasn't entirely sure how to use this website so I am sending the edited code to you through the proposed file changes since this is really the only thing that I can access. Let me know what you think and if you have any suggestions.

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
+
 <?php include("includes/a_config.php");?>
 <!DOCTYPE html>
 <html>
 <head>
-	<?php include("includes/head-tag-contents.php");?>
+	<title>INFODEMIC</title>
+	<link href="style.css" rel="style" type="text/css">
 </head>
+<center>
 <body>
 
 <?php include("includes/design-top.php");?>
 <?php include("includes/navigation.php");?>
 
+<div>
+	<img src="logo19.jpg" width="200px" height="170px" align="left">
+</div>
+
+
 <div class="container" id="main-content">
+	<h1 class="pb-5 pt-3">Infodemic</h1>
 	<h2 class="pb-5 pt-3">COVID-19 Fact-Checking Tool</h2>
 
-	<?php 
+</div>
+	<hr>		
+<div class="main">
+	<ul>
+		<a href="#">MAIN MENU   |  ABOUT  |  NEWS  |  SERVICE </a>
+	</ul>
+</div>
+    <hr>
+    <h1></h1>
+<?php 
 	if (isset($_GET['search'])) {
 		include("includes/search.php");
 	} else {
 		echo '
-
 	<form class="" method="GET">
 		<div class="input-group">
 			<input class="form-control" type="text" name="search" placeholder="Paste URL or enter search terms here"></input>
@@ -26,9 +43,7 @@
 			<button type="submit" class="btn btn-primary mx-auto">Submit</button>
 		</div>
 	</form>
-	';
-	}
-	?>
+	
 
 </div>
 
@@ -39,4 +54,5 @@
 <?php include("includes/footer.php");?>
 
 </body>
+</center>
 </html>
